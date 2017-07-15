3c1d295d8d59dd4af52c6786c399b97f0292b065
jeongjoonsup <penny4860@gmail.com>
2017-07-15 16:59:13 +0900

    done

--- src.py
+++ src.py
@@ -10,42 +10,42 @@ from utils import grayscale, canny, gaussian_blur, region_of_interest, hough_lin
 TEST_DIR = "test_images"
 files = os.listdir(TEST_DIR)
 
-# Read in and grayscale the image
-image = mpimg.imread(os.path.join(TEST_DIR, files[0]))
-imshape = image.shape
-xlength = imshape[1]
-ylength = imshape[0]
-gray = grayscale(image)
+def fine_lane_pipeline(image_path):
+    image = mpimg.imread(image_path)
+    imshape = image.shape
+    xlength = imshape[1]
+    ylength = imshape[0]
+    gray = grayscale(image)
+    
+    # Define a kernel size and apply Gaussian smoothing
+    blur_gray = gaussian_blur(gray, kernel_size=5)
+    
+    # Define our parameters for Canny and apply
+    low_threshold = 50
+    high_threshold = 150
+    edges = canny(blur_gray, low_threshold, high_threshold)
+    
+    # Define the Hough transform parameters
+    # Make a blank the same size as our image to draw on
+    rho = 2
+    theta = np.pi/180
+    threshold = 15
+    min_line_length = 40
+    max_line_gap = 20
+    
+    # Run Hough on edge detected image
+    line_image = hough_lines(edges, rho, theta, threshold, min_line_length, max_line_gap)
+    
+    vertices = np.array([[(0, ylength),
+                          (xlength/2-ylength/10, ylength/2),
+                          (xlength/2+ylength/10, ylength/2),
+                          (xlength, ylength)]], dtype=np.int32)
+    
+    combo = region_of_interest(line_image, vertices)
+    return combo
 
-# Define a kernel size and apply Gaussian smoothing
-blur_gray = gaussian_blur(gray, kernel_size=5)
-
-# Define our parameters for Canny and apply
-low_threshold = 50
-high_threshold = 150
-edges = canny(blur_gray, low_threshold, high_threshold)
-
-# Define the Hough transform parameters
-# Make a blank the same size as our image to draw on
-rho = 2
-theta = np.pi/180
-threshold = 15
-min_line_length = 40
-max_line_gap = 20
-
-# Run Hough on edge detected image
-line_image = hough_lines(edges, rho, theta, threshold, min_line_length, max_line_gap)
-
-vertices = np.array([[(0, ylength),
-                      (xlength/2-ylength/10, ylength/2),
-                      (xlength/2+ylength/10, ylength/2),
-                      (xlength, ylength)]], dtype=np.int32)
-
-combo = region_of_interest(line_image, vertices)
- 
-plt.imshow(image)
-plt.show()
-plt.imshow(combo)
-plt.show()
+for filename in files:
+    im_out = fine_lane_pipeline(os.path.join(TEST_DIR, filename))
+    mpimg.imsave(os.path.join("test_images_output", filename), im_out)
 
 
