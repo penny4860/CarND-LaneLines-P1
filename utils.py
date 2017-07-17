eef3ef18128e89609c322d1e36338109da6ea56e
jeongjoonsup <penny4860@gmail.com>
2017-07-17 21:49:18 +0900

    average_point 에의 한 merge

--- utils.py
+++ utils.py
@@ -76,65 +76,35 @@ def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
         sorted_lines = [lines[i] for i in indexes]
         return sorted_lines
 
-    def get_close_point(line):
+    def average_point(line):
         x1, y1, x2, y2 = line[0, :]
-        if y1 < y2:
-            return x2, y2
-        else:
-            return x1, y1
+        return (x1+x2)/2, (y1+y2)/2
 
-    def get_far_point(line):
-        x1, y1, x2, y2 = line[0, :]
-        if y1 > y2:
-            return x2, y2
-        else:
-            return x1, y1
-
-    
     def merge_line(line1, line2):
         
-        c1 = get_close_point(line1)
-        c2 = get_close_point(line2)
+        x1, y1 = average_point(line1)
+        x2, y2 = average_point(line2)
 
-        f1 = get_far_point(line1)
-        f2 = get_far_point(line2)
-        
-        if c1[1] > c2[1]:
-            close_pt = c1
-        else:
-            close_pt = c2
-
-        if f1[1] < f2[1]:
-            far_pt = f1
-        else:
-            far_pt = f2
-            
-        line = [close_pt[0], close_pt[1], far_pt[0], far_pt[1]]
-        return np.array(line).reshape(-1, 4)
+        line = [x1, y1, x2, y2]
+        return np.array(line).astype(int).reshape(-1, 4)
         
     
     sorted_lines = get_sorted_lines(lines)
     
-    i = 0    
-    while True:
+    for i in range(len(sorted_lines)-1):
         line1 = sorted_lines[i]
         line2 = sorted_lines[i+1]
-         
+          
         s1 = slop(line1)
         s2 = slop(line2)
-         
-        if abs(s1-s2) <= 0.01:
-            sorted_lines[i] = merge_line(line1, line2)
-            sorted_lines.pop(i+1)
-        else:
-            i += 1
-        if i  == len(sorted_lines)-1:
-            break
+          
+        if abs(s1-s2) <= 0.001:
+            sorted_lines.append(merge_line(line1, line2))
 
     for line in sorted_lines:
         for x1,y1,x2,y2 in line:
             # cv2.line(img, (x1, y1), (x2, y2), color, thickness)
-            cv2.line(img, (x1, y1), (x2, y2), color, 10)
+            cv2.line(img, (x1, y1), (x2, y2), color, 5)
             
 
 def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
